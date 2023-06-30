2b628f4075e4f0a81f2abf7fade53a1c4b8a516e
Tiffany Mwangi <tiffany.mwagi@student.moringaschool.com>
2023-06-30 20:06:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+// document.addEventListener("keydown", function (event){
+//     if(event.key === "ArrowLeft") {
+//         const leftnumbers = dodger.style.left.replace("px", "");
+//         const left = parseInt(leftNumbers, 10);
+
+//         dodger.style.left = `${left - 1}`;
+//     }
+// })
+
+
+function moveDodgerLeft() {
+    var dodger = document.getElementById('dodger');
+    var left = parseInt(dodger.style.left) || 0;
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    var dodger = document.getElementById('dodger');
+    var left = parseInt(dodger.style.left) || 0;
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
